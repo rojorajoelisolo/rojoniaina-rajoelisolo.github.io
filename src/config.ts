556c5bb7cb09bbf240ab0bc4dd5e52706270678e
Rojo Rajoelisolo <rojoniaina.rajoelisolo-ext@lcommerce.leclerc>
2026-01-08 15:30:42 +0300

    Initial deploy config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Rojoniaina Rajoelisolo",
+  title: ".NET | Magento 2 Developer",
+  description: "Portfolio website of Rojoniaina Rajoelisolo",
   accentColor: "#1d4ed8",
   social: {
     email: "your-email@example.com",
